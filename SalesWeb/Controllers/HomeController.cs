fffd56bc051675f1a7978c6ea0b0b2caf336616d
Amilton Souza <amiltonsouza099@gmail.com>
2021-07-09 08:22:18 -0300

    modificação no menu de navegação

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace SalesWeb.Controllers {
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
+            ViewData["MyName"] = "Amilton Souza";
 
             return View();
         }
